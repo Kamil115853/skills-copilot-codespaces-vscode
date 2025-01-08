d77225c7d3e551355d40ea533db978c848566ed2
Kamil Dąbek <kamildabek12@gmail.com>
2025-01-08 17:21:51 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,49 @@
+function skillsMember() {
+  var skillsMember = [
+    {
+      name: "HTML",
+      value: 100
+    },
+    {
+      name: "CSS",
+      value: 90
+    },
+    {
+      name: "JavaScript",
+      value: 80
+    },
+    {
+      name: "Node.js",
+      value: 70
+    },
+    {
+      name: "React",
+      value: 60
+    },
+    {
+      name: "Vue.js",
+      value: 50
+    },
+    {
+      name: "Angular",
+      value: 40
+    },
+    {
+      name: "MongoDB",
+      value: 30
+    },
+    {
+      name: "MySQL",
+      value: 20
+    },
+    {
+      name: "PostgreSQL",
+      value: 10
+    },
+    {
+      name: "Python",
+      value: 5
+    }
+  ];
+  return skillsMember;
+}
